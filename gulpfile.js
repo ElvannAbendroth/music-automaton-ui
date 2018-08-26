10d83ddc338d794351675e01dfee0affc3ae61b4
Pekka Astala <pekka.astala@gmail.com>
2018-08-26 16:30:49 +0300

    Switched to compiling mdb scss ourselves

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('css-compile', function() {
-  gulp.src('scss/**/*.scss')
+  gulp.src(['node_modules/mdbootstrap/scss/**/*.scss', 'scss/**/*.scss'])
     .pipe(sass({outputStyle: 'nested'}).on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 10 versions'],
